8f0cee866ae3adf5a03c0ab03577471642dd276c
Aaron Williams <theadwilliams@gmail.com>
2015-06-02 19:18:15 -0700

    Hopper collection view up and running

--- Assassin/HopperViewController.m
+++ Assassin/HopperViewController.m
@@ -7,12 +7,19 @@
 //
 
 #import "HopperViewController.h"
+#import "PlayerCollectionViewCell.h"
 
 @interface HopperViewController () <UITextFieldDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *gameTitleTextField;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *footerConstraint;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+
+
+@property (nonatomic) NSArray *players;
+
 
 @end
 
@@ -29,11 +36,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
+        PFQuery *playerQuery = [[PFQuery alloc] initWithClassName:[Player parseClassName]];
+        [playerQuery whereKey:@"objectId" equalTo:@"mBkKqslY8Y"];
+        NSArray* players = [playerQuery findObjects];
+        self.player = [players firstObject];
     
-    Player *player =[[Player alloc]init];
-    player.host = NO;
-    player.name = @"Aaron";
-    self.player = player;
+        
+        self.game = self.player.game;
+        PFQuery *playersInGame = [[PFQuery alloc] initWithClassName:[Player parseClassName]];
+        [playersInGame whereKey:@"game" equalTo:self.game];
+        [playersInGame findObjectsInBackgroundWithBlock:^(NSArray* results, NSError* error){
+            self.players = results;
+            [self.collectionView reloadData];
+        }];
+
     
     self.gameTitleTextField.delegate = self;
     self.gameTitleTextField.placeholder = [NSString stringWithFormat:@"%@'s Game",self.player.name];
@@ -62,12 +79,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CGSize size = CGSizeMake(self.view.frame.size.width/2, self.view.frame.size.width/2);
+    return size;
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section{
+    return 0;
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
+    return 0;
+}
+
+
 #pragma mark - CollectionView
 
 
 
--(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
     return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.players count];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    PlayerCollectionViewCell *cell =[collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    //    cell.playerImageView.image = [UIImage imageNamed:@"Jer"];
+    //    cell.playerNameLabel.text = @"Jeremy";
+    
+    Player *aPlayer = self.players[indexPath.row];
+    
+    cell.playerNameLabel.text = aPlayer.name;
+    
+    if (aPlayer.deadPhoto) {
+        cell.playerImageView.image = [aPlayer downloadDeadPhoto];
+    }
+    else
+    {
+        [aPlayer.alivePhoto getDataInBackgroundWithBlock:^(NSData* imageData, NSError* error){
+            cell.playerImageView.image = [UIImage imageWithData:imageData];
+        }];
+        
+    }
+    return cell;
 }
 
 /*
